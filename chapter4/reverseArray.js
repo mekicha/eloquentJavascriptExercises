df32891e5d26adec9a388152a5b405d25ceaeffb
mekicha <emeka.icha@gmail.com>
2016-08-12 01:06:22 +0300

    Add author's solution

--- chapter4/reverseArray.js
+++ chapter4/reverseArray.js
@@ -28,3 +28,21 @@ function reverseArrayInPlace(arr){
 		swapArray(arr, i,arr.length -1 -i);
 	}
 }
+
+//Author's solution
+
+function reverseArray(array) {
+  var output = [];
+  for (var i = array.length - 1; i >= 0; i--)
+    output.push(array[i]);
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
