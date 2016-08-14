4a8d0d153f324d297aa5e3fa0908b77fa5aa12b0
mekicha <emeka.icha@gmail.com>
2016-08-14 20:18:08 +0300

    Mother-child age differences

---
+++ chapter5/ageDifference.js
@@ -0,0 +1,25 @@
+/*** Using the example data set from this chapter, compute the
+****  average age difference between mothers and children 
+**** (the age of the mother when the child is born).
+***/
+
+
+
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+var differences = ancestry.filter(function(person) {
+  return byName[person.mother] != null;
+}).map(function(person) {
+  return person.born - byName[person.mother].born;
+});
+
+console.log(average(differences));
